de40ba600f50db47e7f0fbad9582fcaded76d782
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-10-30 22:21:18 -0400

    Created CD Investment

--- src/Account/CDInvestment.java
+++ src/Account/CDInvestment.java
@@ -4,6 +4,13 @@ package Account;
 // has a private field string of timePeriod that uses "M" "Q"
 // can't be withdrawn from.
 
-public class CDInvestment {
+public class CDInvestment extends InvestmentAccount {
+
+    private String timePeriod;
+
+    public CDInvestment(int balance, String owner, String accountNum, String timePeriod) {
+        super(balance, owner, accountNum, timePeriod);
+        this.timePeriod = timePeriod;
+    }
 
 }
