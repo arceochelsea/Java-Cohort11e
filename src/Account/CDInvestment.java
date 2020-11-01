cb3fd3d2e238193729701d2116b763c481d718c8
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-10-31 23:05:22 -0400

    Attempting CDInvestment Class

--- src/Account/CDInvestment.java
+++ src/Account/CDInvestment.java
@@ -1,16 +1,23 @@
 package Account;
 
-// extends Investment
-// has a private field string of timePeriod that uses "M" "Q"
-// can't be withdrawn from.
+// extends Investment yup
+// has a private field string of timePeriod that uses "M" "Q" sorta?
+// can't be withdrawn from. yup
 
 public class CDInvestment extends InvestmentAccount {
 
     private String timePeriod;
 
     public CDInvestment(int balance, String owner, String accountNum, String timePeriod) {
-        super(balance, owner, accountNum, timePeriod);
+        super(balance, owner, accountNum, investment);
         this.timePeriod = timePeriod;
+    }
+
+    public void withdraw() {
+        throw new UnsupportedOperationException();
+        //this will override the specific method and disable the operation!
+        //creating another parent class is also an option.
+        //one that would allow withdraws & perhaps another that doesnt allow withdraws
     }
 
 }
